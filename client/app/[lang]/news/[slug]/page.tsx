c864749ee1e4aa33c484095e38392026e70cc52c
Thilina Ruwan <106557565+thilinaruwan112@users.noreply.github.com>
2024-08-09 19:43:32 +0530

    check image

--- client/app/[lang]/news/[slug]/page.tsx
+++ client/app/[lang]/news/[slug]/page.tsx
@@ -35,7 +35,7 @@ export function generateMetadata({ params }: PageProps): Metadata {
       siteName: "Sri Buzz",
       images: [
         {
-          url: `https://sribuzz.com/og-images/${params.slug}.jpg`,
+          url: `https://images.unsplash.com/photo-1585282263861-f55e341878f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
           width: 800,
           height: 600,
           alt: `${slug} News`,
@@ -48,7 +48,9 @@ export function generateMetadata({ params }: PageProps): Metadata {
       card: "summary_large_image",
       title,
       description,
-      images: [`https://sribuzz.com/og-images/${params.slug}.jpg`],
+      images: [
+        `https://images.unsplash.com/photo-1585282263861-f55e341878f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+      ],
     },
   };
 }
